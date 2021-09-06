63582f41a41959ae3424095e80a8254df94eac0a
datRemi <90219362+datRemi@users.noreply.github.com>
2021-09-06 22:53:41 +0100

    Basic Graphics using Pyxel

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-import pyxel   
+import pyxel
+import math
+
+circleRadius = 10
 
 def main():
-    print("sup brah")
+    pyxel.init(256, 256, caption="First Game", scale=3)
+    pyxel.run(update, draw)
+
+def update():
+    global circleRadius
+    circleRadius = 10 + 10 *(math.sin(pyxel.frame_count))
+    if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+def draw():
+    pyxel.cls(5)
+    pyxel.text(10, 8,"Wasshup", (pyxel.frame_count / 4) % 16)
+    pyxel.circ(128,128, circleRadius, 8)
+    pass
+
 
 main()
